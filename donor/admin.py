f8a130832024ce54d26f4f33faf85cbe93743abe
ydvsahil03 <ydvsahil0003@gmail.com>
2024-04-26 08:56:14 +0530

    first commit

--- donor/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from donor.models import BloodDonate
-
-# Register your models here.
-admin.site.register(BloodDonate)
--- patient/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from patient.models import BloodRequest
-
-# Register your models here.
-admin.site.register(BloodRequest)
